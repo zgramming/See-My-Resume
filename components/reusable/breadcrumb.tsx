59ca00419ad9ef869582997b48f743a554df383d
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-09-12 10:28:20 +0700

    Implement Active Side Menu with scenario single menu and sub menu

--- components/reusable/breadcrumb.tsx
+++ components/reusable/breadcrumb.tsx
@@ -4,7 +4,17 @@ import { useRouter } from "next/router";
 const MyBreadcrum = () => {
 
 	const router = useRouter();
-	const arrPath = router.pathname.split('/').filter(val => val.length !== 0);
+	const arrPath = router.pathname.split('/').filter(route => route.length !== 0).map(route => {
+		const split = route.split("_");
+
+		/// Route => user_group become User Group
+		if (split.length > 1) {
+			const capitalizeName = split.map(val => val[0].toUpperCase() + val.slice(1))
+			return capitalizeName.join(' ');
+		}
+
+		return route;
+	});
 	return <>
 		<Breadcrumb separator=">" className="mb-5">
 			{arrPath.map(val => <Breadcrumb.Item key={val} >{val[0].toUpperCase() + val.slice(1)}</Breadcrumb.Item>)}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 import { ConfigProvider, Layout, Menu, MenuProps } from 'antd';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { PieChartOutlined } from '@ant-design/icons';
 
@@ -15,8 +15,21 @@ import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router';
 const { Content, Footer, Header, Sider } = Layout;
 
+type MenuItem = Required<MenuProps>['items'][number];
+const getItem = (
+	key: React.Key,
+	label: React.ReactNode,
+	icon?: React.ReactNode,
+	children?: MenuItem[]) => {
+	return {
+		key,
+		icon,
+		children,
+		label,
+	} as MenuItem;
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-	const router = useRouter()
 
 	ConfigProvider.config({
 		theme: {
@@ -24,37 +37,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 		},
 	});
 
-	type MenuItem = Required<MenuProps>['items'][number];
-
-	const getItem = (
-		key: React.Key,
-		label: React.ReactNode,
-		icon?: React.ReactNode,
-		children?: MenuItem[]) => {
-		return {
-			key,
-			icon,
-			children,
-			label,
-		} as MenuItem;
-	}
-
-	const sideItems = [
-		getItem('/setting/user', 'Management User', <PieChartOutlined />),
-		getItem('/setting/user_group', 'Management Group User', <PieChartOutlined />),
-		getItem('/setting/modul', 'Modul', <PieChartOutlined />),
-		getItem('/setting/menu', 'Menu', <PieChartOutlined />),
-		getItem('/setting/access_modul', 'Access Modul', <PieChartOutlined />),
-		getItem('/setting/access_menu', 'Access Menu', <PieChartOutlined />),
-		getItem('/setting/master_category', 'Master Kategori', <PieChartOutlined />),
-		getItem('/setting/master_data', 'Master Data', <PieChartOutlined />),
-		getItem('/setting/example', 'Dokumentasi', <PieChartOutlined />),
-		getItem('/setting/parameter', 'Parameter', <PieChartOutlined />),
-		// getItem('7', 'Parent Menu', <PieChartOutlined />, [
-		// 	getItem('7.1', 'Child 1'),
-		// 	getItem('7.2', 'Child 2'),
-		// ]),
-	];
 
 	const headerItems = [
 		getItem('/setting', 'Setting', <PieChartOutlined />),
@@ -76,12 +58,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<div className="relative h-16 flex justify-center items-center" >
 					<Image alt='Failed load image...' src={Logo} height={100} width={100} />
 				</div>
-				<Menu
-					theme="light"
-					mode="inline"
-					items={sideItems}
-					onClick={(e) => router.push(e.key)}
-				/>
+				<SiderMenu />
 			</Sider>
 			<Layout>
 				<Header className='bg-white'>
@@ -101,6 +78,54 @@ function MyApp({ Component, pageProps }: AppProps) {
 		</Layout>
 	</ConfigProvider>
 
+}
+
+const SiderMenu = () => {
+
+	const router = useRouter();
+
+	const currentPathHandler = (path: string): string => {
+		const [first, second, third] = path.split('/').filter(route => route.length !== 0);
+
+		/// We assume when `third` is undefined, this is sub menu
+		return !third ? `/${first}/${second}` : `/${first}/${second}/${third}`;
+	}
+
+	const [currentPath, setCurrentPath] = useState(currentPathHandler(router.pathname));
+
+	const sideItems = [
+		getItem('/setting/user', 'Management User', <PieChartOutlined />),
+		getItem('/setting/user_group', 'Management Group User', <PieChartOutlined />),
+		getItem('/setting/modul', 'Modul', <PieChartOutlined />),
+		getItem('/setting/menu', 'Menu', <PieChartOutlined />),
+		getItem('/setting/access_modul', 'Access Modul', <PieChartOutlined />),
+		getItem('/setting/access_menu', 'Access Menu', <PieChartOutlined />),
+		getItem('/setting/master_category', 'Master Kategori', <PieChartOutlined />),
+		getItem('/setting/master_data', 'Master Data', <PieChartOutlined />),
+		getItem('/setting/example', 'Dokumentasi', <PieChartOutlined />),
+		getItem('/setting/parameter', 'Parameter', <PieChartOutlined />),
+		getItem('?/setting/parent', 'Parent Menu', <PieChartOutlined />, [
+			getItem('/setting/parent/child_first', 'Child 1'),
+			getItem('/setting/parent/child_second', 'Child 2'),
+		]),
+	];
+
+	return <Menu
+		theme="light"
+		mode="inline"
+		items={sideItems}
+		selectedKeys={[currentPath]}
+		onClick={(e) => {
+
+			/// Jangan lakukan push jika character pertama === "?"
+			/// Ini dilakukan untuk meng-akomodir sub menu
+			if (e.key[0] === "?") return false;
+
+			const path = currentPathHandler(e.key);
+			setCurrentPath(path);
+			router.push(path);
+		}}
+	/>
 }
 
 
